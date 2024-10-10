3dba236b8f218cb8971cc728f83967afbe9e9a76
Nicholas Chiu <nicholaschiu@nicholass-mbp.lan>
2024-10-10 10:09:23 -0700

    Adding env variables to test environment

--- workflow_tests/testEnv.js
+++ workflow_tests/testEnv.js
@@ -1,5 +1,18 @@
 require('dotenv').config()
 
-let host = process.env.API_HOST;
-console.log("API_HOST: " + host);
-expect(host, "https://staging-api.lilt.dev");
+(function(root, factory) {
+    if (typeof define === 'function' && define.amd) {
+      // AMD.
+      define(['expect.js', process.cwd()+'/src/index'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+      // CommonJS-like environments that support module.exports, like Node.
+      factory(require('expect.js'), require(process.cwd()+'/src/index'));
+    } else {
+      // Browser globals (root is window)
+      factory(root.expect, root.LiltNode);
+    }
+}(this, function(expect, LiltNode) {
+    let host = process.env.API_HOST;
+    console.log("API_HOST: " + host);
+    expect(host, "https://staging-api.lilt.dev");
+}));
